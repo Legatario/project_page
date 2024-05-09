dfc878f565ccb33568542ea347a9ce23c31f456d
Legatario <l.phillipe.d@gmail.com>
2024-05-09 10:53:42 -0300

    adicionando icons no projeto

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { HeaderComponent } from './../header/header/header.component';
 import { MenuComponent } from './../navBar/menu/menu.component';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from '../pages/home/home.component';
 import { RouterOutlet } from '@angular/router';
 import { CharactersComponent } from '../pages/characters/characters.component';
@@ -8,7 +9,7 @@ import { CharactersComponent } from '../pages/characters/characters.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MenuComponent, HomeComponent, HeaderComponent, RouterOutlet, CharactersComponent],
+  imports: [MatIconModule ,MenuComponent, HomeComponent, HeaderComponent, RouterOutlet, CharactersComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
